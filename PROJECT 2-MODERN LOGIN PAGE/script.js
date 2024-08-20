8ddea8b3f8bec482fa4c8226032ed57f85bc0161
josemills <138175607+josemills@users.noreply.github.com>
2024-08-19 21:04:31 -0400

    Add files via upload

---
+++ PROJECT 2-MODERN LOGIN PAGE/script.js
@@ -0,0 +1,13 @@
+const container = document.getElementById('çontainer');
+const registerBtn = document.
+getElementById('register');
+const loginBtn = document.getElementById
+('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtnBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
